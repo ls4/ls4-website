99885909960fe425c3e16cd6804d5a6f8965fef4
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2011-03-27 22:55:03 +0900

    first commit

---
+++ images-rename.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cp images/images_03.png htdocs/index/splash.png
+cp images/images_06.png htdocs/index/arrow.png
+cp images/images_14.png htdocs/index/button-a.png
+cp images/images_17.png htdocs/index/button-b.png
+cp images/images_11.png htdocs/index/button-back.png
+cp images/images_23.png htdocs/index/headline.png
+cp images/images_27.png htdocs/index/datamodel.png
+cp images/images_31.png htdocs/index/design.png
+cp images/images_35.png htdocs/index/direct.png
+cp images/images_38.png htdocs/index/geo.png
+cp images/images_41.png htdocs/index/shadow.png
+
